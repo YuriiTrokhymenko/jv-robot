9fc7c3ded58f6f614e830dce1f0d3f580fbd664c
Yurii Trokhymenko <uta1965_@ukr.net>
2022-09-15 15:04:51 +0200

    added main

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,41 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        //write your solution here
+
+        // direction of movement along X
+        Direction actualDirection = setDirectionX(robot, toX);
+        while (robot.getDirection() == actualDirection) {
+            robot.turnLeft();
+        }
+        System.out.println(robot);
     }
+
+    // determine the direction of movement X
+    public Direction setDirectionX(Robot robot, int toX) {
+        int actualX = robot.getX();
+        Direction actDirection = robot.getDirection();
+        Direction toDirection = actDirection;
+       if (toX < actualX) {
+            toDirection = Direction.LEFT;
+        } else if (toX > actualX) {
+            toDirection = Direction.RIGHT;
+        }
+        return toDirection;
+    }
+
+    // determine the direction of movement Y
+    public Direction setDirectionY(Robot robot, int toY) {
+        int actualY = robot.getY();
+        Direction actDirection = robot.getDirection();
+        Direction toDirection = actDirection;
+        if (toY < actualY) {
+            toDirection = Direction.DOWN;
+        } else if (toY > actualY) {
+            toDirection = Direction.UP;
+        }
+        return toDirection;
+    }
+
+
 }
